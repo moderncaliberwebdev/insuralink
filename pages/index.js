1d488ce429ddde6bda9bddc7da7fc271f329bb9a
Caleb Martin <cmartin@moderncaliber.com>
2023-05-15 14:47:38 -0400

    graphic and protection sections made

--- pages/index.js
+++ pages/index.js
@@ -55,6 +55,98 @@ export default function Home() {
             />
           </div>
         </header>
+        <section className={styles.home__protection}>
+          <div className={styles.home__protection__left}>
+            <img src='/home/insurance.png' alt='Insurance Graphic' />
+          </div>
+          <div className={styles.home__protection__right}>
+            <h2>Stay protected without any breaks in your insurance</h2>
+            <p>
+              When switching insurance providers, it's crucial not to go a
+              single day without coverage. Even one day with a lapse in coverage
+              could cost hundreds of dollars.
+            </p>
+            <p className={styles.home__protection__right__accent}>
+              When using InsuraLink, we collect necessary information regarding
+              your current provider to aid in cancelling your policy with them
+              on time.
+            </p>
+            <p>
+              Switching your insurance used to be a stressful process... until
+              now. Regardless of what type of insurance you have, start your
+              switch today with
+              <span>
+                Insura<span>Link</span>
+              </span>
+              .
+            </p>
+            <Link href='/start-your-switch'>Start Your Switch</Link>
+          </div>
+        </section>
+        <section className={styles.home__how}>
+          <h2>How It Works</h2>
+          <p className={styles.home__how__subhead}>
+            Three simple steps to switch your insurance.
+          </p>
+          <div className={styles.home__how__graphics}>
+            <div className={styles.home__how__graphics__item}>
+              <img
+                src='/home/line.png'
+                alt='Dashed Line'
+                className={styles.home__how__graphics__item__line}
+              />
+              <div className={styles.home__how__graphics__item__card}>
+                <img src='/home/form.png' alt='Form' />
+                <h4>
+                  Who is your current insurance provider? When would you like
+                  your policy to be cancelled?
+                </h4>
+                <p>
+                  Answer a couple simple questions to provide the information we
+                  need.
+                </p>
+              </div>
+              <h3>1. Fill in your information</h3>
+            </div>
+            <div
+              className={`${styles.home__how__graphics__item} ${styles.item__right}`}
+            >
+              <h3>2. Click Send</h3>
+              <div className={styles.home__how__graphics__item__card}>
+                <img src='/home/send.png' alt='Send graphic' />
+                <h4>Click send and that’s it!</h4>
+                <p>
+                  Once all necessary information is provided and the form is
+                  sent, we'll step in to do the rest.
+                </p>
+              </div>
+              <img
+                src='/home/line.png'
+                alt='Dashed Line'
+                className={styles.home__how__graphics__item__line}
+              />
+            </div>
+            <div className={styles.home__how__graphics__item}>
+              <img
+                src='/home/line.png'
+                alt='Dashed Line'
+                className={styles.home__how__graphics__item__line}
+              />
+              <div className={styles.home__how__graphics__item__card}>
+                <img src='/home/handshake.png' alt='Handshake graphic' />
+                <h4>
+                  InsuraLink connects with your current insurance company.
+                </h4>
+                <p>
+                  We connect with your current insurance company and provide
+                  them with your information. We'll make sure your policy is
+                  cancelled on time.
+                </p>
+              </div>
+              <h3>3. We handle the contact</h3>
+            </div>
+          </div>
+        </section>
       </div>
     </Layout>
   )
