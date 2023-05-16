8ee48a5a6b31a688c0be558c68499833a29eb179
Caleb Martin <cmartin@moderncaliber.com>
2023-05-16 08:36:47 -0400

    footer

--- Components/Layout.js
+++ Components/Layout.js
@@ -39,7 +39,35 @@ export default function Layout({ children }) {
         </div>
       </nav>
       {children}
-      <footer>Footer</footer>
+      <footer className={styles.footer}>
+        <div className={styles.footer__top}>
+          <ul>
+            <Link href='/'>
+              <li className={styles.footer__top__logo}>
+                Insura<span>Link</span>
+              </li>
+            </Link>
+            <Link href='/who-we-are'>
+              <li>Who We Are</li>
+            </Link>
+            <Link href='/our-service'>
+              <li>Our Service</li>
+            </Link>
+            <Link href='/contact-us'>
+              <li>Contact Us</li>
+            </Link>
+            <Link href='/company-portal'>
+              <li>Company Portal</li>
+            </Link>
+            <Link href='/start-your-switch'>
+              <li>Switch Your Insurance</li>
+            </Link>
+          </ul>
+        </div>
+        <div className={styles.footer__bottom}>
+          © Copyright 2023 by InsuraLink
+        </div>
+      </footer>
     </>
   )
 }
